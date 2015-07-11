ddd1e58f863d55b243efe1c36dfa802cef333709
Gilad Levy <giladlevy7@gmail.com>
2015-07-11 18:18:29 +0300

    Bug fixes in scorer

--- main.py
+++ main.py
@@ -3,12 +3,16 @@
 from get_english_synsets import GetEnglishSynsets
 from scorer import SynsetGraph
 
+
 def main():
     es = GetEnglishSynsets()
 
     all_synets = es.get_english_synsets()
 
     graph = SynsetGraph(all_synets)
+
+    for leaf_node in graph.leaf_nodes:
+        print(leaf_node.synset.name() + " " + leaf_node.probability)
 
 if __name__ == '__main__':
     main()
--- scorer.py
+++ scorer.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from nltk.corpus import wordnet as wn
 
 
 class SynsetEdge(object):
@@ -37,16 +36,19 @@ class SynsetNode(object):
     def add_weight(self, weight):
         self.weight += weight
 
-        weight_per_hypernym = weight / float(len(self.hypernym_edges))
-        for hypernym_edge in self.hypernym_edges:
-            hypernym_edge.set_weight(weight_per_hypernym)
+        if len(self.hypernym_edges) != 0:
+            weight_per_hypernym = weight / float(len(self.hypernym_edges))
+            for hypernym_edge in self.hypernym_edges.values():
+                hypernym_edge.set_weight(weight_per_hypernym)
 
     def add_probability(self, probability):
         self.probability += probability
 
-        probability_per_hyponym = probability / float(len(self.hyponym_edges))
-        for hyponym_edge in self.hyponym_edges:
-            hyponym_edge.set_probability(probability_per_hyponym)
+        if len(self.hyponym_edges) != 0:
+            probability_per_hyponym = (probability /
+                                       float(len(self.hyponym_edges)))
+            for hyponym_edge in self.hyponym_edges.values():
+                hyponym_edge.set_probability(probability_per_hyponym)
 
     def add_hypernym(self, hypernym_node):
         hypernym_name = hypernym_node.synset.name()
@@ -64,54 +66,55 @@ class SynsetGraph(object):
 
         leaf_synsets = set(synset_weight_dict.keys())
 
-        hypernym_paths = {hypernym_path for hypernym_path in synset.hypernym_paths()
-                                        for synset in leaf_synsets}
-        synsets = {synset for synset in hypernym_path for hypernym_path in hypernym_paths}
-        synset_nodes = {synset : SynsetNode(synset) for synset in synsets}
+        hypernym_paths = [hypernym_path for
+                          synset in leaf_synsets for
+                          hypernym_path in synset.hypernym_paths()]
+        print(hypernym_paths)
+        synsets = {synset for hypernym_path in
+                   hypernym_paths for synset in hypernym_path}
+        print(synsets)
+        synset_nodes = {synset: SynsetNode(synset) for synset in synsets}
 
         for leaf in leaf_synsets:
-            leaf_node = synset_nodes[leaf_node]
-            
+            leaf_node = synset_nodes[leaf]
+
             for hypernym_path in leaf.hypernym_paths():
-                for i in xrange(0,len(hypernym_path - 1)):
-                    
+                for i in range(1, len(hypernym_path)):
+
                     hyponym_node = synset_nodes[hypernym_path[i]]
-                    hypernym_node = synset_nodes[hypernym_path[i + 1]]
+                    hypernym_node = synset_nodes[hypernym_path[i - 1]]
 
                     hyponym_node.add_hypernym(hypernym_node)
 
         for leaf in leaf_synsets:
-            leaf_node = synset_nodes[leaf_node]
+            leaf_node = synset_nodes[leaf]
             leaf_node.add_weight(synset_weight_dict[leaf])
 
-        entity_synset = [synset for synset in synsets if synset.name() == "entity"][0]
+        entity_synset = [synset for
+                         synset in synsets if
+                         synset.name() == "entity.n.01"][0]
         entity_node = synset_nodes[entity_synset]
 
         entity_node.add_probability(1)
 
         self.synset_nodes = synset_nodes
-
-
-
-
-
-
-
+        self.leaf_nodes = {self.synset_nodes[synset]
+                           for synset in leaf_synsets}
 
 
 class SynsetProperties(object):
     """docstring for SynsetProperties"""
     def __init__(self, synset):
         super(SynsetProperties, self).__init__()
-        
+
         self.synset = synset
         self.reference_count = 0
         self.avg_depth = (self.synset.min_depth() + self.synset.max_depth()) / float(2)
-    
+
     def increment_reference_count(self, synset_weight):
         self.reference_count += synset_weight
 
-    #def __str__(self):
+    # def __str__(self):
     #   return ("name: {}\n".format(self.synset.name()) +
     #           "reference_count: {}\n".format(self.reference_count) +
     #           "avg_depth: {}\n".format(self.avg_depth) +
@@ -120,11 +123,12 @@ class SynsetProperties(object):
     def __repr__(self):
         return self.synset.name()
 
+
 class SynsetScorer(object):
     """docstring for SynsetScorer"""
     def __init__(self):
         super(SynsetScorer, self).__init__()
-        
+
         self.synset_dict = {}
 
     def _add_hypernym_path_synsetss(self, hypernym_path, synset_weight):
@@ -138,31 +142,12 @@ class SynsetScorer(object):
             self._add_hypernym_path(hypernym_path, synset_weight)
 
     def sort_synsets_by(self, key_func, reverse=True, display=False):
-        sorted_synsets = sorted(self.synset_dict.values(), key=key_func, reverse=reverse)
-        
+        sorted_synsets = sorted(self.synset_dict.values(),
+                                key=key_func, reverse=reverse)
+
         if display:
             for sp in sorted_synsets:
                 print(sp)
                 print('\n')
 
         return sorted_synsets
-
-def main():
-   scorer = SynsetScorer()
-
-   dog_synsets = wn.synsets("dog")
-   cat_synsets = wn.synsets("cat")
-   mouse_synsets = wn.synsets("mouse")
-   horse_synsets = wn.synsets("horse")
-
-   dog = dog_synsets[0]
-   cat = cat_synsets[0]
-   mouse = mouse_synsets[0]
-   horse = horse_synsets[0]
-
-   scorer.add_hypernym_paths(dog)
-   scorer.add_hypernym_paths(cat)
-   scorer.add_hypernym_paths(mouse)
-   scorer.add_hypernym_paths(horse)
-
-   print(scorer.sort_synsets_by(lambda sp: sp.reference_count * sp.avg_depth))
